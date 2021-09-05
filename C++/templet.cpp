2cbf7693dd82b0fe15b5c1c15361d3e3f749ef6e
gabrielfrimodig <gabriel.frimodig@student.uu.se>
2021-09-05 08:07:01 +0200

    templet

---
+++ C++/templet.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+
+void test_case(){
+
+}
+
+int main(){
+	int T;
+	scanf("%d", &T);
+	for(int z = 1; z <= T; z++) {
+		test_case();
+	}
+
+	return 0;
+}
