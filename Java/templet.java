c0d9a8a72274a83db8e87ebd73446b5197bafca8
gabrielfrimodig <gabriel.frimodig@student.uu.se>
2021-09-05 07:55:50 +0200

    templet

---
+++ Java/templet.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        //String word = in.next();
+
+        int T = in.nextInt();
+
+        for(int i = 1; i <= T ; i++){
+
+        }
+
+    }
+}
